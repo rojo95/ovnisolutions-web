63fa814eac84db9f49c633a52cc4c74113caf13d
Alexander <r.johan95@gmail.com>
2022-08-14 04:15:49 -0400

    Actualización de botones del formulario stepper final; en el carousel se colocaron los títulos en la etiqueta h1; se creó el boton de cambio de idioma en la web; se agregaron los estilos para el boton de cambio de idioma; en el componente menu se importaron los archivos necesarios para el boton de cambio de idioma; se declaró el formulario y la asignación de datos en el constructor junto con funciones que serán usadaspara el cambio de idioma;

--- src/app/views/menu/menu.component.ts
+++ src/app/views/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormBuilder } from '@angular/forms';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -11,13 +12,27 @@ export class MenuComponent implements OnInit {
   bars = faBars;
   show:boolean = false;
 
-  constructor() { }
+  public langForm = this.formBuilder.group({
+    langControl: new FormControl (localStorage.getItem('lang')||'1', [Validators.required])
+  });
+
+  constructor(private formBuilder: FormBuilder) {
+    if(!localStorage.getItem('lang')){
+      localStorage.setItem('lang','1');
+    }
+  }
 
   ngOnInit(): void {
   }
 
   actionMenu() {
     this.show = !this.show
+  }
+
+  changeLang(){}
+
+  getLanguage(){
+
   }
 
 }
