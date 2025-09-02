07a46398f2a73202bd723ed37dddc0bd0fbbac86
Johan Román <r.johan95@gmail.com>
2025-09-02 17:05:12 -0400

    cambiar correo oficial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  officialEmail: 'ovnisolutions@ovnisolutions.com',
+  officialEmail: 'contacto@ovnisolutions.com',
   backend: 'api.ovnisolutions.com',
   appToken: '%NcyKc!9_1WERvqWc,Fv:?1R6RkderDFM!w&$-Xh',
 };
